b77baf254d70a845a7686263c5c511034912e9cf
Diego Soares <soaressd01@gmail.com>
2022-05-04 11:14:40 -0300

    Teste de checagem de medicamentos

--- pages/api/checkdrugs/index.js
+++ pages/api/checkdrugs/index.js
@@ -6,25 +6,25 @@ export default function CheckDrugs(req, res) {
     }
 
     const drugs = req.body.drugs;
-    /*if (drugs) {
+    if (drugs) {
         if (drugs.length > 1) {
 
             const base_url = `https://rxnav.nlm.nih.gov/REST/interaction/list.json?rxcuis=${drugs.replaceAll(';', '+')}`;
+            res.status(200).send(base_url);
 
-            fetch(base_url)
+            /*fetch(base_url)
             .then((response) => {
                 return response.json();
             })
             .then((response) => {
                 res.status(200).json(response.fullInteractionTypeGroup);
-            })
+            });*/
 
         } else {
             res.status(405).json({ error: 'Passe dois ou mais medicamentos para a checagem.' });
         }
     } else {
         res.status(405).json({ error: 'Passe dois ou mais medicamentos para a checagem.' });
-    }*/
-    res.status(200).send(drugs);
+    }    
 
 }
