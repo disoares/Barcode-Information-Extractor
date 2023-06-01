b14a4f7cec410054aab3a70a1fbbf21a2d4165ef
Diego Soares <soaressd01@gmail.com>
2023-06-01 16:38:22 -0300

    Teste1

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 export default function Home() {
-  const a = fetch('http://api2.valoz.app.br/get-payment-status/1684497997433').then(response => response.json()).then(json => console.log(json))
+  const a = fetch('https://api2.valoz.app.br/get-payment-status/1684497997433').then(response => response.json()).then(json => console.log(json))
     .catch((err) => {
       console.log(err);
     });
